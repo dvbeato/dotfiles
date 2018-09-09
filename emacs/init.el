c2e7f94922c74e3a1fab14f4bfea31ffda22d148
dvbeato <diogo.vecchiati@gmail.com>
2018-09-09 16:18:22 -0300

    update emacs settings

--- emacs/init.el
+++ emacs/init.el
@@ -3,10 +3,16 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-(line-number-mode t)
-(column-number-mode t)
-(size-indication-mode t)
-(whitespace-mode t)
+(global-hl-line-mode t)
+(global-linum-mode t)
+(setq column-number-mode t)
+(setq size-indication-mode t)
+(setq indent-tabs-mode nil)
+(setq tab-width 2)
+(setq visible-bell t)
+(setq ring-bell-function 'ignore)
+(setq show-paren-mode t)
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; No splash screen
 (setq inhibit-startup-message t)
@@ -31,12 +37,8 @@
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
        (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t))
 (package-initialize)
 
 ;; update the package metadata is the local cache is missing
@@ -49,19 +51,108 @@
 (require 'use-package)
 (setq use-package-verbose t)
 
+(use-package magit
+  :ensure t)
+
+(use-package flatland-theme
+  :ensure t
+  :config (load-theme 'flatland t))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode))
+
+(use-package paredit
+  :ensure t
+  :bind
+  (("M-{" . paredit-wrap-curly)
+   ("M-<" . paredit-wrap-angled)
+   ("M-[" . paredit-wrap-square))
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+  ;; enable in the *scratch* buffer
+  (add-hook 'lisp-interaction-mode-hook #'paredit-mode)
+  (add-hook 'ielm-mode-hook #'paredit-mode)
+  (add-hook 'lisp-mode-hook #'paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode))
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package neotree
+  :ensure t
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
+
 (use-package evil
   :ensure t
-  :init (evil-mode t))
+  :init (evil-mode t)
+  :config
+;;  (evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "SPC") 'neotree-quick-look)
+;;  (evil-define-key 'normal neotree-mode-map (kbd "q") 'neotree-hide)
+  (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+  (evil-define-key 'normal neotree-mode-map (kbd "g") 'neotree-refresh)
+  (evil-define-key 'normal neotree-mode-map (kbd "n") 'neotree-next-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "p") 'neotree-previous-line)
+  (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
+  (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
+  )
 
-(use-package zenburn-theme
+(use-package helm
+  :ensure t)
+
+(use-package projectile
   :ensure t
-  :config (load-theme 'zenburn t))
+  :bind (("C-c s" . projectile-switch-open-project)
+  ("C-x p" . projectile-switch-project))
+  :config
+  (projectile-global-mode)
+  (setq projectile-enable-caching t))
+
+(use-package helm-projectile
+  :ensure t
+  :bind ("M-p" . helm-projectile-find-file)
+  :config
+  (helm-projectile-on))
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook #'paredit-mode)
+  (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode))
+
+(use-package cider
+  :ensure t
+  :config
+  (setq nrepl-log-messages t)
+  (add-hook 'cider-repl-mode-hook #'paredit-mode)
+  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode))
+
+;; whitespace settings
+(global-whitespace-mode t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (zenburn-theme evil use-package))))
+ '(package-selected-packages
+   (quote
+    (all-the-icons flatland-theme magit neotree helm-projectile helm projectile cider clojure-mode paredit rainbow-delimiters zenburn-theme evil use-package)))
+ '(whitespace-display-mappings
+   (quote
+    ((space-mark 32
+		 [183]
+		 [46])
+     (space-mark 160
+		 [164]
+		 [95])
+     (tab-mark 9
+	       [187 9]
+	       [92 9])))))
+(set-face-attribute 'whitespace-space nil :background nil :foreground "gray20")
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
