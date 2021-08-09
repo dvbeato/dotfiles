17bd3f79f427cde002ef35cd9a5ba272e8130fe5
beato <diogo.beato@nubank.com.br>
2021-08-09 09:52:59 -0300

    update

--- vscode/install.sh
+++ vscode/install.sh
@@ -5,12 +5,12 @@ while read p; do
   code --install-extension $p
 done < $PWD/extensions.conf
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  ln -s $HOME/.dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
-  ln -s $HOME/.dotfiles/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
-fi
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-ln -s $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-ln -s $HOME/.dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-fi
+#if [[ "$OSTYPE" == "linux-gnu" ]]; then
+#  ln -s $HOME/.dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
+#  ln -s $HOME/.dotfiles/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+#fi
+#
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+#ln -s $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+#ln -s $HOME/.dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+#fi
