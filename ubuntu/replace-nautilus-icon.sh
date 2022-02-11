4299de3581453e9e1836842663c93d73690b1d3c
dvbeato <diogo.beato@nubank.com.br>
2022-02-11 15:11:01 -0300

    update

--- ubuntu/replace-nautilus-icon.sh
+++ ubuntu/replace-nautilus-icon.sh
@@ -4,7 +4,7 @@ cd /usr/share/icons/Yaru
 
 for f in $(find -name "*Nautilus*"); do
   folder_size=$(echo "$f" | sed s/\\/apps\\/org.gnome.Nautilus.\*//g)
-  user_home=$folder_size/places/user-home.png
+  user_home=$folder_size/places/folder.png
   echo "copying $user_home to $f"
   sudo cp $user_home $f
 done;
