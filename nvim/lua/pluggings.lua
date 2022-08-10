ef4f58ec6d1d03f32e794d7ca79a75de584b890a
dvbeato <diogo.beato@nubank.com.br>
2022-08-10 18:56:34 -0300

    fix nvim-treesitter config

--- nvim/lua/pluggings.lua
+++ nvim/lua/pluggings.lua
@@ -65,7 +65,8 @@ require'nvim-tree'.setup {
 
 -- NvimTreesitter
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "c", "lua", "clojure", "go", "javascript", "hcl" },
+  auto_install = true,
   highlight = {
     enable = true,              -- false will disable the whole extension
     -- disable = { "c", "rust" },  -- list of language that will be disabled
